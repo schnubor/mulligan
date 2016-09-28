bc6d8b315ce6b4737ef7f2b652bc82536f71a1ec
schnubor <info@chko.org>
2016-09-29 01:43:52 +0200

    linter fixed

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,22 +1,22 @@
 <template>
-  <div class="hello">
+<div class="hello">
     <h1>{{ msg }}</h1>
-  </div>
+</div>
 </template>
 
 <script>
 export default {
-  data () {
-    return {
-      msg: 'Hello Vue!'
+    data () {
+        return {
+            msg: 'Hello Vue!'
+        }
     }
-  }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 h1 {
-  color: #42b983;
+    color: #42b983;
 }
 </style>
