d093a97eea89b272ee7ca33b1fdbf4ad212631b5
Christian <christian@styla.com>
2016-09-29 18:31:44 +0200

    new styles

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,8 @@
 <template>
 <div>
-    <nav class="navbar navbar-fixed-top navbar-dark bg-inverse">
+    <nav class="navbar navbar-fixed-top navbar-light bg-faded">
         <div class="container" id="exCollapsingNavbar2">
-            <router-link class="navbar-brand" to="/">Brand</router-link>
-            <ul class="nav navbar-nav">
-                <li class="nav-item active">
-                    <router-link class="nav-link" to="/user/schnubor">User</router-link>
-                </li>
-                <li class="nav-item">
-                    <router-link class="nav-link" to="/card/1">Card</router-link>
-                </li>
-                <li class="nav-item">
-                    <router-link class="nav-link" to="/deck/2">Deck</router-link>
-                </li>
-            </ul>
+            <router-link class="navbar-brand" to="/">Mulligan</router-link>
         </div>
     </nav>
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,15 @@
 <template lang="html">
     <div class="jumbotron">
-        <div class="container">
-            <h1 class="display-3">Hello, world!</h1>
-            <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+        <div class="container text-xs-center">
+            <p class="lead">Find your card</p>
+            <form>
+                <div class="form-group">
+                    <input name="name" class="form-control-lg form-control" placeholder="Card name" v-model="name">
+                </div>
+                <div class="form-group">
+                    <button type="button" class="btn btn-lg btn-outline-secondary">Find Cards</button>
+                </div>
+            </form>
         </div>
     </div>
 </template>
@@ -10,7 +17,9 @@
 <script>
 export default {
     data () {
-        return {}
+        return {
+            name: ''
+        }
     },
     computed: {},
     ready () {},
@@ -20,5 +29,19 @@ export default {
 }
 </script>
 
-<style lang="css">
+<style lang="css" scoped>
+    .jumbotron {
+        background-image: url('../assets/images/bg1.jpg');
+        background-repeat: no-repeat;
+        background-size: cover;
+        border-radius: 0;
+    }
+
+    .lead {
+        color: white;
+        font-size: 2em;
+        text-transform: uppercase;
+        font-weight: 100;
+        letter-spacing: 6px;
+    }
 </style>
