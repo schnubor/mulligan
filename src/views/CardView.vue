d52cccb575561951ee70e59707b46278afe90f9a
Christian <christian@styla.com>
2016-10-07 18:31:23 +0200

    replace card test with symbols

--- src/views/CardView.vue
+++ src/views/CardView.vue
@@ -71,7 +71,17 @@ export default {
             }
         },
         rawText () {
-            return this.card.text.replace(/\n/g, '<br />')
+            let text = this.card.text
+            text = text.replace(/\n/g, '<br />')
+            text = text.replace(/\{T\}/g, '<i class="ms ms-tap ms-cost"></i>')
+            text = text.replace(/\{R\}/g, '<i class="ms ms-r ms-cost"></i>')
+            text = text.replace(/\{U\}/g, '<i class="ms ms-u ms-cost"></i>')
+            text = text.replace(/\{G\}/g, '<i class="ms ms-g ms-cost"></i>')
+            text = text.replace(/\{W\}/g, '<i class="ms ms-w ms-cost"></i>')
+            text = text.replace(/\{B\}/g, '<i class="ms ms-b ms-cost"></i>')
+            text = text.replace(/\{E\}/g, '<i class="ms ms-e"></i>')
+            text = text.replace(/\{(\d)\}/g, '<i class="ms ms-$1 ms-cost"></i>')
+            return text
         }
     },
     methods: {
